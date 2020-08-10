52daf4d6294e7304ac5120c2c273ce95f3c7fab1
Felipe Milana <felipe.milana97@gmail.com>
2020-08-10 16:31:59 -0300

    Little improvement in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -27,9 +27,20 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {   //inicia a partida e coloca as peças no tabuleiro
-		placeNewPiece('b', 6, new Rook(board, Color.WHITE));  //instancia uma peça, com o tabuleiro e cor, e sua posição
-		placeNewPiece('e', 8, new King(board, Color.BLACK));
-		placeNewPiece('e', 1, new King(board, Color.WHITE));		
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));  //instancia uma peça, com o tabuleiro e cor, e sua posição
+		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 2, new Rook(board, Color.WHITE));	
+		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 1, new King(board, Color.WHITE));
+		
+		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 8, new King(board, Color.BLACK));
+	
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
